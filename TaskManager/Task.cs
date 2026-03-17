f4308df3cc1d7ae7188fcb2ff0a3fa18d0f02cf0
Андросова <and.kris0509@gmail.com>
2026-03-17 11:52:54 +0300

    Добавлен новый класс

---
+++ TaskManager/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    public class Task
+    {
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Task(string description)
+        {
+            Description = description;
+            IsCompleted = false;
+        }
+    }
+}
